847d5396566b32d6128b2f7e62e63ceed73308c0
Eryk Neri <eryk.ner@gmail.com>
2023-12-28 16:32:01 -0300

    Handling search error and site select menu

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
 
-
-
 (async () => {
   // Launch the browser and open a new blank page | Inicie o navegador e abra uma nova página em branco
   const browser = await puppeteer.launch({headless: false});
@@ -17,22 +15,35 @@ const readlineSync = require('readline-sync');
   // Set screen size | Definir tamanho da tela
   await page.setViewport({width: 1980, height: 1024});
 
+  // Asks if the user wants to use search correction | Pergunta se o usuário deseja usar a correção de pesquisa
+  try {
 
-  // Locate the full title with a unique string
-  const Element = await page.waitForSelector('.MjjYud');
+    //Searches for element within 5000 milliseconds (1 seconds) | Procura elemnto dentro de 1000 milissegundos (1 segundos)
+    const element = await page.waitForSelector('#fprsl', { timeout: 1000 });
+    
+    do{  
+      const Correcao = readlineSync.question('Deseja corrigir a sua pesquisa para "'+ await element?.evaluate(el => el.innerText) + '"? [S] [N] ');
+      Verificacao = (Correcao.toUpperCase()=='S'||Correcao.toUpperCase()=='N');
+
+      if(Correcao.toUpperCase()=='S'){
+        await page.click('#fprsl');
+      }else if(Correcao.toUpperCase()!='N'){
+        console.log('Opção invalida')
+      }
+      
+    }while(!Verificacao);
+  } catch (error) {
+    console.error('Elemento não encontrado:', error);
+  }
 
   const Data = await page.evaluate( ()=>{
     
-    let allElments = document.querySelectorAll('.MjjYud');
+    let allElments = document.querySelectorAll('[jscontroller=SC7lYd]');
     arr = [];
 
     allElments.forEach(item =>{
       let TitleSite = item.querySelector('.yuRUbf a h3');
       let URLSite = item.querySelector('.yuRUbf a');
-      
-      if(TitleSite == null){
-        item.style.background = 'red';
-      }
 
       arr.push({
         Title : TitleSite ? TitleSite.innerText : null,
@@ -42,28 +53,14 @@ const readlineSync = require('readline-sync');
         
     return arr;
   });
-  console.log(Data);
+
+  //Let the user choose an item from a list | Deixe o usuário escolher um item de uma lista
+  const index = readlineSync.keyInSelect(Data.map((item)=>{return item.Title +" link:"+ item.Link}), 'Qual site?');
+  console.log(Data[index]);
 
   // Screenshot | Captura de tela
   await page.screenshot({path: 'example.png', fullPage: true});
   
-  
-  // Type into search box
-  //await page.type('.search-box__input', 'automate beyond recorder');
 
-  // Wait and click on first result
-  /*const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    '.MjjYud'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-*/
   await browser.close();
 })();
